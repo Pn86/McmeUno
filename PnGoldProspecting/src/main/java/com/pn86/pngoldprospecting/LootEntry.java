d3b98153a4aac9d0352209a3971458059a723af3
Pn86 <3999389202@qq.com>
2026-03-29 10:56:10 +0800

    Delete PnGoldProspecting directory

--- PnGoldProspecting/src/main/java/com/pn86/pngoldprospecting/LootEntry.java
+++
@@ -1,9 +0,0 @@
-package com.pn86.pngoldprospecting;
-
-import org.bukkit.inventory.ItemStack;
-
-public record LootEntry(String itemId, ItemStack itemStack, int weight, String command) {
-    public boolean isCommandLoot() {
-        return command != null && !command.isBlank();
-    }
-}
