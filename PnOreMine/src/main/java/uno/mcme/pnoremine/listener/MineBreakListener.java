183b82ea98fd9139814ac0edf605804e97fad4a7
Pn86 <3999389202@qq.com>
2026-04-03 09:41:42 +0800

    fix: restore mine reward payout for normal block breaking

--- PnOreMine/src/main/java/uno/mcme/pnoremine/listener/MineBreakListener.java
+++ PnOreMine/src/main/java/uno/mcme/pnoremine/listener/MineBreakListener.java
@@ -4,8 +4,11 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
+import org.bukkit.event.block.BlockDropItemEvent;
 import uno.mcme.pnoremine.PnOreMinePlugin;
+import uno.mcme.pnoremine.mine.DropMode;
 import uno.mcme.pnoremine.mine.MineRegion;
+import uno.mcme.pnoremine.mine.OreEntry;
 
 public class MineBreakListener implements Listener {
 
@@ -15,15 +18,32 @@ public class MineBreakListener implements Listener {
         this.plugin = plugin;
     }
 
-    @EventHandler(priority = EventPriority.HIGHEST, ignoreCancelled = true)
-    public void onBreak(BlockBreakEvent event) {
+    @EventHandler(priority = EventPriority.MONITOR, ignoreCancelled = true)
+    public void onDrop(BlockDropItemEvent event) {
+        MineRegion mine = plugin.getMineManager().findMineByLocation(event.getBlockState().getLocation());
+        if (mine == null) {
+            return;
+        }
+        OreEntry ore = mine.getOreEntry(event.getBlockState().getType());
+        if (ore == null) {
+            return;
+        }
+
+        if (mine.getDropMode() == DropMode.VALUE || mine.getDropMode() == DropMode.ITEM) {
+            event.getItems().clear();
+        }
+        plugin.rewardPlayerForOre(event.getPlayer(), mine, event.getBlockState().getType(), event.getBlockState().getLocation().add(0.5, 0.5, 0.5));
+    }
+
+    @EventHandler(priority = EventPriority.MONITOR, ignoreCancelled = true)
+    public void onBreakFallback(BlockBreakEvent event) {
+        if (event.isDropItems()) {
+            return;
+        }
         MineRegion mine = plugin.getMineManager().findMineByLocation(event.getBlock().getLocation());
         if (mine == null) {
             return;
         }
-        boolean rewarded = plugin.rewardPlayerForOre(event.getPlayer(), mine, event.getBlock().getType(), event.getBlock().getLocation());
-        if (rewarded) {
-            event.setDropItems(false);
-        }
+        plugin.rewardPlayerForOre(event.getPlayer(), mine, event.getBlock().getType(), event.getBlock().getLocation().add(0.5, 0.5, 0.5));
     }
 }
